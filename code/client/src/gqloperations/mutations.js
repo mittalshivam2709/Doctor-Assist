645e2edf5ad0f06f3700191ff4ff8b7045e061b6
bassamadnan <bassamadnan2012@gmail.com>
2024-02-20 16:46:47 +0530

    add jwt to frontend

---
+++ code/client/src/gqloperations/mutations.js
@@ -0,0 +1,11 @@
+import {gql} from '@apollo/client'
+
+export const LOGIN_USER = gql`
+mutation LoginUser($userInput: UserInput!) {
+  loginUser(userInput: $userInput) {
+    username
+    password
+    token
+  }
+}
+`
