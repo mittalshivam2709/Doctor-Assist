91180a885aaff7593cb483f0822992f1288ea7ab
mittalshivam2709 <shivam.mittal@students.iiit.ac.in>
2024-03-18 15:11:28 +0530

    signup login frontend changes

--- code/client/src/pages/LoginPage.js
+++ code/client/src/pages/LoginPage.js
@@ -1,30 +1,30 @@
-import { useState,useEffect } from "react";
-import { useForm } from "react-hook-form";
-import { LOGIN_USER } from "../gqloperations/mutations";
-import { useMutation } from "@apollo/client";
-import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from 'react'
+import { useForm } from 'react-hook-form'
+import { LOGIN_USER } from '../gqloperations/mutations'
+import { useMutation } from '@apollo/client'
+import { useNavigate } from 'react-router-dom'
 import '../loginpage.css'
-import {Link } from "react-router-dom";
+import { Link } from 'react-router-dom'
 
 const LoginPage = () => {
-  const { register, handleSubmit,reset } = useForm();
-  const [data, setData] = useState("");
-  const navigate = useNavigate();
+  const { register, handleSubmit, reset } = useForm()
+  const [data, setData] = useState('')
+  const navigate = useNavigate()
   const [logininUser, { error, loading, formdata }] = useMutation(LOGIN_USER, {
     onCompleted: (formdata) => {
-      console.log(formdata.loginUser);
+      console.log(formdata.loginUser)
       // localStorage.setItem("token",formdata.user.token)
       // console.log(formdata.user.token);
-      navigate("/home");
+      navigate('/home')
     },
     onError: (error) => {
-      alert(error.message);
-      reset();
-    }
-  });
+      alert(error.message)
+      reset()
+    },
+  })
   const onSubmit = (data) => {
-    console.log("Form data submitted:", data);
-    setData(JSON.stringify(data));
+    console.log('Form data submitted:', data)
+    setData(JSON.stringify(data))
     logininUser({
       variables: {
         userInput: {
@@ -32,8 +32,8 @@ const LoginPage = () => {
           password: data.password,
         },
       },
-    });
-  };
+    })
+  }
   // useEffect(() => {
   //   if (error) {
   //     alert(error.message);
@@ -41,22 +41,46 @@ const LoginPage = () => {
   // }, [error]);
   return (
     <div className="outerlogin">
-    <div className="login-container">
-      <h2 style={{color:"blue", position:'relative',top:'-20px',fontSize:'30px'}}>Login Page</h2>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <input {...register("username")} placeholder="Username" />
-        <br />
-        <input {...register("password")} type="password" placeholder="Password" />
-        <br />
-        <input type="submit" value="Login" />
-      </form>
-      <Link to='/signup' className='btn btn-default border w-100 bg-light rounded-0 text-decoration-none'>
-          Signup
-         </Link>
+      <div className="login-container">
+        <h2
+          style={{
+            color: 'blue',
+            position: 'relative',
+            top: '-20px',
+            fontSize: '30px',
+          }}
+        >
+          Login Page
+        </h2>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <input {...register('username')} placeholder="Username" />
+          <br />
+          <input
+            {...register('password')}
+            type="password"
+            placeholder="Password"
+          />
+          <br />
+          <input type="submit" value="Login" />
+          <p>New user ?</p>
+          <Link
+            to="/signup"
+            type="button"
+            className="btn btn-default border w-100 bg-light rounded-0 text-decoration-none"
+            style={{
+              background: '#007bff',
+              color: 'white',
+              backgroundSize: '40px',
+              fontSize: '20px',
+              textAlign: 'center',
+            }}
+          >
+            Signup
+          </Link>
+        </form>
+      </div>
     </div>
-  </div>
-  
-  );
-};
+  )
+}
 
-export default LoginPage;
+export default LoginPage
--- code/client/src/pages/SignupPage.js
+++ code/client/src/pages/SignupPage.js
@@ -3,7 +3,6 @@
 // import Axios from "axios"
 // import {Link } from "react-router-dom";
 
-
 // const handleSubmit = (e) => {
 //   e.preventDefault()
 //   Axios.port('https://localhost:3000/auth/signup', {
@@ -62,33 +61,31 @@
 
 // export default SignupPage
 
-
-
-import { useState,useEffect } from "react";
-import { useForm } from "react-hook-form";
-import { SIGNUP_USER } from "../gqloperations/mutations";
-import { useMutation } from "@apollo/client";
-import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from 'react'
+import { useForm } from 'react-hook-form'
+import { SIGNUP_USER } from '../gqloperations/mutations'
+import { useMutation } from '@apollo/client'
+import { useNavigate } from 'react-router-dom'
 import '../pages/SignupPage.css'
-import {Link } from "react-router-dom";
+import { Link } from 'react-router-dom'
 
 const SignupPage = () => {
-  const { register, handleSubmit,reset } = useForm();
-  const [data, setData] = useState("");
-  const navigate = useNavigate();
+  const { register, handleSubmit, reset } = useForm()
+  const [data, setData] = useState('')
+  const navigate = useNavigate()
   const [signupUser, { error, loading, formdata }] = useMutation(SIGNUP_USER, {
     onCompleted: (formdata) => {
       // localStorage.setItem("token",formdata.user.token)
-      navigate("/home");
+      navigate('/home')
     },
     onError: (error) => {
-      alert(error.message);
-      reset();
-    }
-  });
+      alert(error.message)
+      reset()
+    },
+  })
   const onSubmit = (data) => {
-    console.log("Form data submitted:", data);
-    setData(JSON.stringify(data));
+    console.log('Form data submitted:', data)
+    setData(JSON.stringify(data))
     signupUser({
       variables: {
         userInput: {
@@ -97,11 +94,11 @@ const SignupPage = () => {
           doctor_name: data.doctor_name,
           doctor_degree: data.doctor_degree,
           doctor_mobile: data.doctor_mobile,
-          doctor_visit: data.doctor_visit
+          doctor_visit: data.doctor_visit,
         },
       },
-    });
-  };
+    })
+  }
   // useEffect(() => {
   //   if (error) {
   //     alert(error.message);
@@ -109,30 +106,54 @@ const SignupPage = () => {
   // }, [error]);
   return (
     <div className="outerlogin">
-    <div className="login-container">
-      <h2 style={{color:"blue", position:'relative',top:'-20px',fontSize:'30px'}}>Signup Page</h2>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <input {...register("username")} placeholder="Username" />
-        <br />
-        <input {...register("password")} type="password" placeholder="Password" />
-        <br />
-        <input {...register("doctor_name")} placeholder="Doctor's Name" />
-        <br />
-        <input {...register("doctor_degree")} placeholder="Doctor's Degree" />
-        <br />
-        <input {...register("doctor_mobile")} placeholder="Doctor's Mobile" />
-        <br />
-        <input {...register("doctor_visit")} placeholder="Doctor's visit" />
-        <br />
-        <input type="submit" value="Signup" />
-      </form>
-      <Link to='/ ' className='btn btn-default border w-100 bg-light rounded-0 text-decoration-none'>
-          Login
-         </Link>
+      <div className="login-container">
+        <h2
+          style={{
+            color: 'blue',
+            position: 'relative',
+            top: '-20px',
+            fontSize: '30px',
+          }}
+        >
+          Signup Page
+        </h2>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <input {...register('username')} placeholder="Username" />
+          <br />
+          <input
+            {...register('password')}
+            type="password"
+            placeholder="Password"
+          />
+          <br />
+          <input {...register('doctor_name')} placeholder="Doctor's Name" />
+          <br />
+          <input {...register('doctor_degree')} placeholder="Doctor's Degree" />
+          <br />
+          <input {...register('doctor_mobile')} placeholder="Doctor's Mobile" />
+          <br />
+          <input {...register('doctor_visit')} placeholder="Doctor's visit" />
+          <br />
+          <input type="submit" value="Signup" />
+          <p>Already have an account?</p>
+          <Link
+            to="/"
+            type="button"
+            className="btn btn-default border w-100 bg-light rounded-0 text-decoration-none"
+            style={{
+              background: '#007bff',
+              color: 'white',
+              backgroundSize: '40px',
+              fontSize: '20px',
+              textAlign: 'center',
+            }}
+          >
+            Login
+          </Link>
+        </form>
+      </div>
     </div>
-  </div>
-  
-  );
-};
+  )
+}
 
-export default SignupPage;
+export default SignupPage
