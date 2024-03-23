891cadeb9012d4b64e288de354a7ce98425c27ca
mayankmittal29 <mayank.mittal@students.iiit.ac.in>
2024-03-23 06:08:30 +0530

    criticality judgement with css done only sorting left

--- code/client/src/components/Dropdown.js
+++ code/client/src/components/Dropdown.js
@@ -11,12 +11,130 @@ const Dropdown = ({ data }) => {
     gender,
     critical_case,
     emt,
+    body_temperature,
+    blood_pressure_sys,
+    blood_pressure_dys,
+    pulse_rate,
+    spo2,
   } = data;
-
+  const judgeCriticality = () =>
+  {
+    let criticality = ''
+    if (critical_case === "yes")
+    {
+      criticality = "Critical";
+    }
+    else 
+    {
+      let count_critical = 0;
+      let count_moderate = 0;
+      let count_minor = 0;
+      // For body temperature
+      if(( body_temperature >= 105) || (body_temperature<=95))
+      {
+        count_critical=count_critical+1;
+      }
+      else if((body_temperature >=103) && (body_temperature<105))
+      {
+        count_moderate = count_moderate+1;
+      }
+      else if ((body_temperature <103))
+      {
+        count_minor=count_minor +1;
+      }
+      // For Spo2
+      if(spo2<85)
+      {
+        count_critical=count_critical+1;
+      }
+      else if((spo2>=85)&&(spo2<=90))
+      {
+        count_moderate = count_moderate+1;
+      }
+      else if ((spo2 >90))
+      {
+        count_minor=count_minor +1;
+      }
+      // For Heart rate
+      if(( pulse_rate >= 130) || (pulse_rate<=40))
+      {
+        count_critical=count_critical+1;
+      }
+      else if((pulse_rate >=110) && (pulse_rate<=60))
+      {
+        count_moderate = count_moderate+1;
+      }
+      else if ((pulse_rate <110) && (pulse_rate>60))
+      {
+        count_minor=count_minor +1;
+      }
+      // For Systolic blood pressure
+      if(( blood_pressure_sys >= 180) || (blood_pressure_sys<=90))
+      {
+        count_critical=count_critical+1;
+      }
+      else if((blood_pressure_sys >90) && (blood_pressure_sys<=160))
+      {
+        count_minor= count_minor+1;
+      }
+      else if ((blood_pressure_sys <180) && (blood_pressure_sys>160))
+      {
+        count_moderate=count_moderate +1;
+      }
+      // for diastolic pressure
+      if(( blood_pressure_dys >= 120) || (blood_pressure_dys<=80))
+      {
+        count_critical=count_critical+1;
+      }
+      else if((blood_pressure_dys >=90) && (blood_pressure_dys<=120))
+      {
+        count_moderate= count_moderate+1;
+      }
+      else if ((blood_pressure_dys <90) && (blood_pressure_dys>80))
+      {
+        count_minor=count_minor +1;
+      }
+      if(count_critical>=3)
+      {
+        criticality= 'Critical';
+      }
+      else if((count_critical==2)&&(count_moderate>=2))
+      {
+        criticality='Moderate';
+      }
+      else if(count_moderate>0)
+      {
+        criticality='Moderate';
+      }
+      else
+      {
+        criticality='Minor'
+      }
+    }
+    return criticality;
+  };
+  const criticality = judgeCriticality();
+  let hoverColor = '';
+  let hoverC = '';
+  if (criticality == 'Critical')
+  {
+   hoverColor = "#EB164D";
+   hoverC = "#FFD9E2"
+  }
+  else if (criticality == 'Moderate')
+  {
+    hoverColor = "#FAA416";
+    hoverC = "#FBEBCF"
+  }
+  else if (criticality == 'Minor')
+  {
+   hoverColor = "#01B48E";
+   hoverC = "#D2FAF1"
+  }
   const [hovered, setHovered] = useState(false);
-  const [showProblemDetails, setShowProblemDetails] = useState(false);
-  const hoverColor = critical_case === "yes" ? "rgb(244, 183, 183)" : "rgb(169, 235, 169)";
-  const hoverC = critical_case === "yes" ? "red" : "green";
+  // const [showProblemDetails, setShowProblemDetails] = useState(false);
+  // const hoverColor = critical_case === "yes" ? "rgb(244, 183, 183)" : "rgb(169, 235, 169)";  // 
+  // const hoverC = critical_case === "yes" ? "red" : "green";                                    // ambulance no color+dot
   const { selectedChat, setSelectedChat, setSelectedPatient } = ChatState();
   const isSelected = selectedChat === emt;
   var color = "white"
@@ -27,9 +145,9 @@ const Dropdown = ({ data }) => {
 
   const handleHover = (isHovered) => {
     setHovered(isHovered);
-    setShowProblemDetails(isHovered);
+    // setShowProblemDetails(isHovered);
   };
-
+  
   return (
     <div
       className={`expanding-box bg-${isSelected ? hoverColor : "white"} text-${isSelected ? "white" : "black"}`}
@@ -44,7 +162,7 @@ const Dropdown = ({ data }) => {
             ? `linear-gradient(to right, ${color} 100%, #FFFFFF 100%)`
             : `linear-gradient(to right, ${color} 2.5%, #FFFFFF 2.5%)`,
           border: isSelected ? "2px solid blue" : "2px solid transparent",
-          background: hovered? hoverColor:"white",
+          background: hovered? hoverC:"white",
           backgroundColor: "transparent"
         }}
       >
@@ -58,22 +176,22 @@ const Dropdown = ({ data }) => {
           <div className=" flex flex-col md:flex-row justify-between">
             
             <div className=" flex items-center">
-            <div  style={{ width: '20px', height: '20px', borderRadius: '50%', position: 'relative', right: '8px',backgroundColor: hoverC }}></div>
-            <div className=""  style={{ color: hovered ? hoverC : "blue" ,width :'200px', fontWeight:600}}>Ambulance No: {ambulance_no}</div>
+            <div  style={{ width: '20px', height: '20px', borderRadius: '50%', position: 'relative', right: '8px',backgroundColor: hoverColor}}></div>
+            <div className=""  style={{ color: hovered ? hoverColor : "#5555FB" ,width :'200px', fontWeight:600}}>Ambulance No: {ambulance_no}</div>
             </div>
-            <div className="text-xs" >{admit_time}</div>
+            <div className="text-xs" style={{color:"#929292"}} >{admit_time}</div>
           </div>
           <div className="flex py-2 items-center" style={{fontWeight:400}}>
             <span
-              className="problem_patient"
+              className="problem_patient" style={{color:"#6F6B6B"}}
             >
-              {showProblemDetails ? problem : "What is the problem?"}
+              {problem}
             </span>
           </div>
           <div className="flex justify-between text-sm" style={{fontWeight: 400}}>
-            <span className="age_patient text-xs"> Age - {age} years</span>
-            <span className="agetype_patient text-xs"> Age type - {age_type}</span>
-            <span className="gender_patient text-xs"> Gender - {gender}</span>
+            <span className="age_patient text-xs" style={{color:"#929292"}}> Age - {age} years</span>
+            <span className="agetype_patient text-xs" style={{color:"#929292"}}> Age type - {age_type}</span>
+            <span className="gender_patient text-xs" style={{color:"#929292"}}> Gender - {gender}</span>
           </div>
         </div>
       </div>
--- code/client/src/components/Dropdown2.js
+++ code/client/src/components/Dropdown2.js
@@ -3,7 +3,7 @@ import { ChatState } from '../context/ChatProvider'
 
 const Dropdown2 = ({ data }) => {
   const {
-    name,
+    ambulance_no,
     admit_time,
     problem,
     age,
@@ -11,13 +11,132 @@ const Dropdown2 = ({ data }) => {
     gender,
     critical_case,
     emt,
+    body_temperature,
+    blood_pressure_sys,
+    blood_pressure_dys,
+    pulse_rate,
+    spo2,
+    name
   } = data
-
+  const judgeCriticality = () =>
+  {
+    let criticality = ''
+    if (critical_case === "yes")
+    {
+      criticality = "Critical";
+    }
+    else 
+    {
+      let count_critical = 0;
+      let count_moderate = 0;
+      let count_minor = 0;
+      // For body temperature
+      if(( body_temperature >= 105) || (body_temperature<=95))
+      {
+        count_critical=count_critical+1;
+      }
+      else if((body_temperature >=103) && (body_temperature<105))
+      {
+        count_moderate = count_moderate+1;
+      }
+      else if ((body_temperature <103))
+      {
+        count_minor=count_minor +1;
+      }
+      // For Spo2
+      if(spo2<85)
+      {
+        count_critical=count_critical+1;
+      }
+      else if((spo2>=85)&&(spo2<=90))
+      {
+        count_moderate = count_moderate+1;
+      }
+      else if ((spo2 >90))
+      {
+        count_minor=count_minor +1;
+      }
+      // For Heart rate
+      if(( pulse_rate >= 130) || (pulse_rate<=40))
+      {
+        count_critical=count_critical+1;
+      }
+      else if((pulse_rate >=110) && (pulse_rate<=60))
+      {
+        count_moderate = count_moderate+1;
+      }
+      else if ((pulse_rate <110) && (pulse_rate>60))
+      {
+        count_minor=count_minor +1;
+      }
+      // For Systolic blood pressure
+      if(( blood_pressure_sys >= 180) || (blood_pressure_sys<=90))
+      {
+        count_critical=count_critical+1;
+      }
+      else if((blood_pressure_sys >90) && (blood_pressure_sys<=160))
+      {
+        count_minor= count_minor+1;
+      }
+      else if ((blood_pressure_sys <180) && (blood_pressure_sys>160))
+      {
+        count_moderate=count_moderate +1;
+      }
+      // for diastolic pressure
+      if(( blood_pressure_dys >= 120) || (blood_pressure_dys<=80))
+      {
+        count_critical=count_critical+1;
+      }
+      else if((blood_pressure_dys >=90) && (blood_pressure_dys<=120))
+      {
+        count_moderate= count_moderate+1;
+      }
+      else if ((blood_pressure_dys <90) && (blood_pressure_dys>80))
+      {
+        count_minor=count_minor +1;
+      }
+      if(count_critical>=3)
+      {
+        criticality= 'Critical';
+      }
+      else if((count_critical==2)&&(count_moderate>=2))
+      {
+        criticality='Moderate';
+      }
+      else if(count_moderate>0)
+      {
+        criticality='Moderate';
+      }
+      else
+      {
+        criticality='Minor'
+      }
+    }
+    return criticality;
+  };
+  const criticality = judgeCriticality();
+  let hoverColor = '';
+  let hoverC = '';
+  if (criticality == 'Critical')
+  {
+   hoverColor = "#EB164D";
+   hoverC = "#FFD9E2"
+  }
+  else if (criticality == 'Moderate')
+  {
+    hoverColor = "#FAA416";
+    hoverC = "#FBEBCF"
+  }
+  else if (criticality == 'Minor')
+  {
+   hoverColor = "#01B48E";
+   hoverC = "#D2FAF1"
+  }
   const [hovered, setHovered] = useState(false)
-  const [showProblemDetails, setShowProblemDetails] = useState(false)
-  const hoverColor =
-    critical_case === 'yes' ? 'rgb(244, 183, 183)' : 'rgb(169, 235, 169)'
-  const hoverC = critical_case === 'yes' ? 'red' : 'green'
+  // const [showProblemDetails, setShowProblemDetails] = useState(false)
+  // const hoverColor =
+  //   critical_case === 'yes' ? 'rgb(244, 183, 183)' : 'rgb(169, 235, 169)'
+  // const hoverC = critical_case === 'yes' ? 'red' : 'green'
   const { selectedChat, setSelectedChat, setSelectedPatient } = ChatState()
   const isSelected = selectedChat === emt
   var color = 'white'
@@ -28,7 +147,6 @@ const Dropdown2 = ({ data }) => {
 
   const handleHover = (isHovered) => {
     setHovered(isHovered)
-    setShowProblemDetails(isHovered)
   }
 
   return (
@@ -49,7 +167,7 @@ const Dropdown2 = ({ data }) => {
             ? `linear-gradient(to right, ${color} 100%, #FFFFFF 100%)`
             : `linear-gradient(to right, ${color} 2.5%, #FFFFFF 2.5%)`,
           border: isSelected ? '2px solid blue' : '2px solid transparent',
-          background: hovered ? hoverColor : 'white',
+          background: hovered ? hoverC : 'white',
           backgroundColor: 'transparent',
         }}
       >
@@ -69,13 +187,13 @@ const Dropdown2 = ({ data }) => {
                   borderRadius: '50%',
                   position: 'relative',
                   right: '8px',
-                  backgroundColor: hoverC,
+                  backgroundColor: hoverColor,
                 }}
               ></div>
               <div
                 className=""
                 style={{
-                  color: hovered ? hoverC : 'blue',
+                  color: hovered ? hoverColor : 'blue',
                   width: '10px',
                   fontWeight: 600,
                 }}
