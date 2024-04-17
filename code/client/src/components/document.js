0a701c6f9a7babd267ebfd55a039f56a6d7957e4
mittalshivam2709 <shivam.mittal@students.iiit.ac.in>
2024-04-17 08:59:14 +0530

    frontend setup for change activity status

--- code/client/src/components/document.js
+++ code/client/src/components/document.js
@@ -4,39 +4,44 @@ import docion from '../docicon.png'
 import { DELETE_DOCUMENT } from '../gqloperations/mutations'
 import deleteicon from '../delete.png'
 import { useMutation } from '@apollo/client'
+import { Link } from 'react-router-dom' // Import Link from react-router-dom
 
 import '../admin.css'
 // similar to dropdownjs
 
 // const Document = ({}) => {
 const Document = ({ data }) => {
-  const { admin_email, document_url, document_no, document_name, admit_time } =
+  const { admin_email, document_url, document_no, document_name,active_to_train, admit_time } =
     data
-    
-    let hoverColor = ''
-    let hoverC = ''
-    const [hovered, setHovered] = useState(false)
-    
-    // const { selectedChat, setSelectedChat, setSelectedPatient } = ChatState()
-    // const isSelected = selectedChat === emt
-    var color = 'white'
-    
-    const handleToggle = () => {
-      setSelectedChat(isSelected ? null : emt)
-      setSelectedPatient(isSelected ? null : data)
-    }
-    
-    const handleHover = (isHovered) => {
-      setHovered(isHovered)
-      // setShowProblemDetails(isHovered);
-    }
 
+  let hoverColor = ''
+  let hoverC = ''
+  const [hovered, setHovered] = useState(false)
 
-  const handletoggleforactive = () => {}
-  
-  const handledelete = () => {
-  
+  // const { selectedChat, setSelectedChat, setSelectedPatient } = ChatState()
+  // const isSelected = selectedChat === emt
+  var color = 'white'
+
+  const handleToggle = () => {
+    setSelectedChat(isSelected ? null : emt)
+    setSelectedPatient(isSelected ? null : data)
   }
+
+  const handleHover = (isHovered) => {
+    setHovered(isHovered)
+    // setShowProblemDetails(isHovered);
+  }
+
+  const [dropdownOpen, setDropdownOpen] = useState(false)
+
+  const toggleDropdown = () => {
+    setDropdownOpen(!dropdownOpen)
+  }
+
+  const changeactivityastatus = (status) => {
+    
+  }
+  const handledelete = () => {}
   return (
     <div className="parentdocument">
       <div className="left">
@@ -49,53 +54,41 @@ const Document = ({ data }) => {
           <img src={deleteicon} alt="Image 1" />
         </button>
         <button>
-          <div
-            className="threedots"
-            onClick={handletoggleforactive} // Add onClick event to handle button click
-          >
-            <div className="onedot"></div>
-            <div className="onedot"></div>
-            <div className="onedot"></div>
-          </div>
+          <button onClick={toggleDropdown}>
+            <div className="threedots">
+              <div className="onedot"></div>
+              <div className="onedot"></div>
+              <div className="onedot"></div>
+            </div>
+          </button>
+
+          {dropdownOpen && (
+            <div className="absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg z-10">
+              <button
+                className={`block w-full text-left px-4 py-2 text-sm ${
+                  active_to_train === '1'
+                    ? 'bg-gray-200 text-gray-900'
+                    : 'text-gray-700 hover:bg-gray-100 hover:text-gray-900'
+                }`}
+                onClick={changeactivityastatus('1')}
+              >
+                Active to Train
+              </button>
+              <button
+                className={`block w-full text-left px-4 py-2 text-sm ${
+                  active_to_train === '0'
+                    ? 'bg-gray-200 text-gray-900'
+                    : 'text-gray-700 hover:bg-gray-100 hover:text-gray-900'
+                }`}
+                onClick={changeactivityastatus('0')}
+              >
+                Inactive to Train
+              </button>
+            </div>
+          )}
         </button>
       </div>
     </div>
-
-    // <div className="parentdocument">
-    //   <img
-    //     style={{ width: '40px', height: '40px' }}
-    //     src={docion}
-    //     alt="document-icon"
-    //   />
-    //   <p style={{ whiteSpace: 'nowrap' }}>
-    //     {document_name} {admit_time}
-    //   </p>
-    //   {/* Remove single quotes around placeholders */}
-    //   <button
-    //     onClick={handledelete}
-    //     style={{
-    //       marginLeft: '950px',
-    //     }}
-    //   >
-    //     <img
-    //       style={{
-    //         width: '40px',
-    //         height: '40px',
-    //       }}
-    //       src={deleteicon}
-    //       alt="delete-icon"
-    //     />
-    //   </button>
-    //   {/* button for 1 and 0 */}
-    //   <div
-    //     className="threedots"
-    //     onClick={handletoggleforactive} // Add onClick event to handle button click
-    //   >
-    //     <div className="onedot"></div>
-    //     <div className="onedot"></div>
-    //     <div className="onedot"></div>
-    //   </div>
-    // </div>
   )
 }
 
