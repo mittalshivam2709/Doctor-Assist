bb84de20a30a8e48b53bf6556eeffa171e3d8742
udaybindal01 <uday.bindal@research.iiit.ac.in>
2024-04-19 17:42:01 +0530

    words

--- code/client/src/components/Message.js
+++ code/client/src/components/Message.js
@@ -7,15 +7,17 @@ import doc from "../doc.svg"
 const Message = ({ message, right }) => {
   console.log(right)
   return (
-    <div className="flex flex-row chat-parent" style={{ width:"100%", alignItems: "center" }}>
+    <div className="flex flex-row chat-parent" style={{ width:"100%" }}>
       {right == 0 && (
         <div
           style={{
             flex: "0.05 0 5%",
             display: "flex",
             flexDirection: "column",
-            justifyContent: "center",
+            // justifyContent: "flex-start",
+            // alignItems: "flex-start"
             // backgroundColor:"yellow",
+            // marginTop:"0px"
             // margin:"3px"
           }}
         >
@@ -26,7 +28,7 @@ const Message = ({ message, right }) => {
           />
           
         </div>
-        
+        // </div>
       )}
       
      
@@ -37,9 +39,10 @@ const Message = ({ message, right }) => {
             flex: "0.05 0 5%",
             display: "flex",
             flexDirection: "column",
-            justifyContent: "center",
-            // marginRight:"1px",
-            // marginLeft:"3px"
+            justifyContent: "flex-start",
+            marginRight:"5px",
+            marginLeft:"3px",
+            // marginTop:"10px"
             // backgroundColor:"red"
           }}
         >
@@ -51,8 +54,10 @@ const Message = ({ message, right }) => {
         </div>
         
       )}
-       <div className="chat-box" style={right == 1 ? {wordBreak:"break-word",backgroundColor:"transparent",padding:"10px",paddingLeft:"20px",border: "1.3px solid blue",margin:"5px",borderRadius:"7px" }:{width:"100%", wordBreak:"break-word",backgroundColor:"transparent",padding:"3px",margin:"1px",borderRadius:"7px"}}>
-        <div className={`chat-message ${right == 0 ? "left" : "right"}`} style={right == 1 ?{color:"blue"} : {}}>
+       <div className="chat-box" style={right == 1 ? {wordBreak:"break-word",backgroundColor:"transparent" }:{width:"100%", wordBreak:"break-word",backgroundColor:"transparent",padding:"3px",margin:"1px",marginTop:"15px",borderRadius:"7px"}}>
+        {right == 0 ? <p style={{marginLeft:"15px",marginBottom:"5px"}}>Knowledge Base</p>: <p style={{marginLeft:"15px",marginBottom:"5px"}}>You</p>}
+        
+        <div className={`chat-message ${right == 0 ? "left" : "right"}`} style={right == 1 ?{color:"blue",border: "1.3px solid blue",padding:"10px",paddingLeft:"20px",margin:"5px",borderRadius:"7px"} : {}}>
           {message}
         </div>
       </div>
