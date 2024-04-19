bcb3d516c3c6e86429cd6e0de8ad3610440bb035
mayankmittal29 <mayank.mittal@students.iiit.ac.in>
2024-04-20 01:25:50 +0530

    sender of message in chat fixed

--- code/client/src/components/MessageInput.js
+++ code/client/src/components/MessageInput.js
@@ -41,10 +41,11 @@ const MessageInput = () => {
       setInputText(file.name);
     }
   };
-
+  const authdata = JSON.parse(localStorage.getItem('authdata'));
+  const idsender = authdata ? authdata.id : '';
   const { register, handleSubmit, reset } = useForm();
   const [selectedFile, setSelectedFile] = useState(null);
-  const { user, selectedChat, setMessage } = ChatState();
+  const { selectedChat, setMessage } = ChatState();
   const [sendMessage] = useMutation(SEND_MESSAGE);
   const customSubmit = async (data) => {
     if(selectedFile){
@@ -73,7 +74,7 @@ const MessageInput = () => {
               }
             const messageData = {
               content: fileUrl,
-              sender: user,
+              sender: idsender,
               receiver: selectedChat,
               type:"image"
             }
@@ -98,7 +99,7 @@ const MessageInput = () => {
       if (data.Message.trim() !== '') {
         const messageData = {
           content: data.Message,
-          sender: user,
+          sender: idsender,
           receiver: selectedChat,
         }
         sendMessage({
